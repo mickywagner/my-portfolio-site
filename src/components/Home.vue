72e3bb8f6f84d1430b8d65f23cf32f0834d12bbf
Michaela Wagner <mickywagner@gmail.com>
2020-07-16 13:46:01 -0600

    Change HelloWorld to Home, remove styles, add temp bg

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Michaela Wagner</h1>
+    <p>Full Stack Developer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  background: url('../assets/tempBG.jpg');
+  background-size: cover;
+}
+
+</style>
