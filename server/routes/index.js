1c133e629c433080d8bc58deac9756797ec66061
Michaela Wagner <mickywagner@gmail.com>
2020-08-20 16:02:45 -0600

    Nodemailer post route works

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs')
-var path = 
+var nodemailer = require('nodemailer')
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +18,40 @@ router.get('/', function(req, res, next) {
 
 router.get('/resume', function(req, res, next) {
   res.redirect('/static/resume.pdf')
+})
+
+router.post('/email', function(req, res, next) {
+  const { firstname, lastname, email, text } = (req.body)
+
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${firstname} ${lastname}</li>
+      <li>Email: ${email}</li>
+    </ul>
+    <h3>Message</h3>
+    ${text}
+  `
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: 'Contact from Portfolio Site',
+    html: output
+  }
+
+  console.log(mailOptions, output)
+ 
+  transporter.sendMail(mailOptions, function(error, info) {
+      if(error) {
+          res.send({message: error})
+      } else {
+          res.send('email sent ' + info.response)
+      }
+  }) 
+ 
+
 })
 
 module.exports = router;
